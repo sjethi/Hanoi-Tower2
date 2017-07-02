0931f872857fc3c5396b3a0eaaa33ca2b834dd51
sjethi <saarthi.jethi@gmail.com>
2017-07-02 11:19:56 -0700

    Add files via upload
    Used base cases to automatically predict a specific block to its corresponding tower.

---
+++ hanoihelper.py
@@ -0,0 +1,18 @@
+n = int(raw_input("Please input the level of tower:\n"))
+
+# function for hanoi tower
+
+def hanoi(n):
+	hanoiHelper(n, 'TowerA', 'TowerB', 'TowerC')
+
+def hanoiHelper(n, origin, helper, destination):
+	if n == 1: #base case
+		print 'Move disk', n, 'from', origin, 'to', destination
+	else: # recursive case
+			hanoiHelper(n-1, origin, destination, helper)
+			print'Move disk', n, 'from', origin, 'to', destination
+			hanoiHelper(n-1, helper, origin, destination)
+
+#test function
+hanoi(n)
+
